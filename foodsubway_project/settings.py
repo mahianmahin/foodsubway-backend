bb3050faedb7440700f79aae15fda35d22e3f04e
mahianmahin <mahianmahin@yahoo.com>
2022-06-01 05:35:05 +0600

    modified

--- foodsubway_project/settings.py
+++ foodsubway_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
-    'corsheaders',
+    # 'corsheaders',
     # 'rest_framework_simplejwt',
 ]
 
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    # "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
 
